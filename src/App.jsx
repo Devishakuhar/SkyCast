46d609150f09978f0616d53bc6ade8935e2230c2
Devisha <devishakuhar@gmail.com>
2025-02-14 15:20:08 +0530

    Updated feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-import LikeButton from './LikeButton.jsx';
-import Todo from './Todo.jsx';
-import TempMsgBox from './TempMsgBox.jsx'; // Import the component with the correct name
 import './App.css';
-import Form from './Form.jsx';
-import SearchBox from './SearchBox.jsx';
 import WeatherApp from './WeatherApp.jsx';
 function App() {
   
